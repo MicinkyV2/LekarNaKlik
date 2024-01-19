72913807c13683a4375387d0a4a67d931f188b0c
Slimey-dev <djakub10g@gmail.com>
2024-01-19 20:10:46 +0100

    Update Mapbox height in index.astro

--- src/components/Mapbox.tsx
+++ src/components/Mapbox.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 
 mapboxgl.accessToken = import.meta.env.PUBLIC_MAPBOX_KEY;
 
-export default function App({ height }: { height: number }) {
+export default function App({ height }: { height: string }) {
 	const mapContainer = useRef<HTMLDivElement | null>(null);
 	const map: React.MutableRefObject<mapboxgl.Map | null> = useRef(null);
 	const [lng] = useState(15.5);
@@ -61,6 +61,46 @@ export default function App({ height }: { height: number }) {
 		}
 	}
 
+	class FullscreenControl implements mapboxgl.IControl {
+		private container: HTMLDivElement;
+
+		constructor() {
+			this.container = document.createElement("div");
+			this.container.className =
+				"pointer-events-auto flex flex-col items-center mt-2 ml-2 bg-white rounded-lg overflow-hidden shadow-md border border-gray-200 hover:border-gray-300";
+			this.container.style.fontSize = "18px";
+		}
+
+		onAdd(map: mapboxgl.Map): HTMLElement {
+			const fullscreenButton = document.createElement("button");
+			fullscreenButton.innerHTML = `<img src="/public/mapUI/corners-out.svg" style="display: block; margin-left: auto; margin-right: auto; width: 25px">`;
+			fullscreenButton.style.width = "35px";
+			fullscreenButton.style.height = "35px";
+			fullscreenButton.addEventListener("click", () => {
+				if (document.fullscreenElement) {
+					document.exitFullscreen();
+				} else {
+					map.getContainer().requestFullscreen();
+				}
+			});
+			fullscreenButton.addEventListener("mouseover", () => {
+				fullscreenButton.style.backgroundColor = "#F5F5F5";
+			});
+			fullscreenButton.addEventListener("mouseout", () => {
+				fullscreenButton.style.backgroundColor = "";
+			});
+
+			this.container.appendChild(fullscreenButton);
+
+			return this.container;
+		}
+
+		onRemove(): void {
+			this.container.parentNode?.removeChild(this.container);
+			this.container = null!;
+		}
+	}
+
 	class LocationControl implements mapboxgl.IControl {
 		private container: HTMLDivElement;
 
@@ -75,7 +115,7 @@ export default function App({ height }: { height: number }) {
 			const locationButton = document.createElement("button");
 			locationButton.innerHTML = `<img src="/public/mapUI/navigation-arrow.svg" style="display: block; margin-left: auto; margin-right: auto; width: 20px">`;
 			locationButton.style.width = "35px";
-			locationButton.style.height = "40px";
+			locationButton.style.height = "35px";
 			locationButton.addEventListener("click", () => {
 				if (navigator.geolocation) {
 					navigator.geolocation.getCurrentPosition((position) => {
@@ -183,11 +223,13 @@ export default function App({ height }: { height: number }) {
 		});
 
 		const combinedControl = new CombinedControl();
+		const fullscreenControl = new FullscreenControl();
 		map.current?.addControl(combinedControl, "top-right");
+		map.current?.addControl(fullscreenControl, "top-left");
 	});
 
 	return (
-		<div style={{ height: `${height}px` }}>
+		<div style={{ height: `${height}` }}>
 			<div ref={mapContainer} className="map-container w-full h-full" />
 		</div>
 	);
