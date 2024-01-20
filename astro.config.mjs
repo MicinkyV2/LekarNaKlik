0ef1b7eccaca1f568444546d2e6b48b0f7cb67a2
Kedlub <jakubkyzr@gmail.com>
2024-01-20 12:37:28 +0100

    Node server backend

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
-	output: "server",
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
