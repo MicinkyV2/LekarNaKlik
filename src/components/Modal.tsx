81cd8f67a1b6cb374c8194ef42daf89b76020cf0
Slimey-dev <djakub10g@gmail.com>
2024-01-20 10:24:11 +0100

    chatbot modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,46 +1,91 @@
+import {
+	Avatar,
+	Button,
+	Card,
+	CardBody,
+	CardHeader,
+	Input,
+	Modal,
+	ModalBody,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	useDisclosure,
+} from "@nextui-org/react";
+import type { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 import React from "react";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input} from "@nextui-org/react";
+
+let messages: ChatCompletionMessageParam[] = [];
+let inputText: string;
+messages.push({
+	role: "assistant",
+	content:
+		"Jsi chatbot, který má za úkol poradit uživatelům k jakému typu doktora si mají zajít se svým problémem, podle jejich specializace. Svoje odpovědi ponechej stručné, ale stále podej alespoň nějaké informace nevíc a nechoď zbytečne do detailů pokud to uživatel nechce. Nabídni uživateli více informací.",
+});
+
+async function Chat(text: string) {
+	messages.push({
+		role: "user",
+		content: text,
+	});
+	const response = await fetch(`/api/chat`, {
+		role: "user",
+		content: text,
+	});
+	const newData = (await response.text()) as string;
+	console.log(newData);
+	const parsedData = JSON.parse(newData);
+
+	messages.push({
+		role: "assistant",
+		content: "parsedData",
+	});
+}
 
 export default function App() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+	const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-  return (
-    <>
-      <Button onPress={onOpen}>AI</Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">AI chat</ModalHeader>
-              <ModalBody>
-                <p> 
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
-                  dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. 
-                  Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. 
-                  Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur 
-                  proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Input client:visible placeholder="Zde napište svoji zprávu" />
-                <Button client:visible color="success" auto className="py-7 ">
-                  <img src="/paper-plane-right.svg" alt="Odeslat"/>
-                  </Button> 
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
-  );
+	return (
+		<>
+			<Button onPress={onOpen}>AI</Button>
+			<Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+				<ModalContent>
+					{(onClose) => (
+						<>
+							<ModalHeader className="flex flex-col gap-1">AI chat</ModalHeader>
+							<ModalBody>
+								<Card shadow="sm">
+									<CardHeader className="pb-0">
+										<Avatar showFallback />
+										<h2 className="pl-2 text-lg">AI</h2>
+									</CardHeader>
+									<CardBody className="pt-1"></CardBody>
+								</Card>
+								<Card shadow="sm">
+									<CardHeader className="justify-end pb-0">
+										<h2 className="pr-2 text-lg">Vy</h2>
+										<Avatar showFallback />
+									</CardHeader>
+									<CardBody className="pt-1"></CardBody>
+								</Card>
+							</ModalBody>
+							<ModalFooter>
+								<Input
+									placeholder="Zde napište svoji zprávu"
+									value={inputText}
+								/>
+								<Button
+									color="success"
+									className="py-7"
+									onClick={() => Chat(inputText)}
+								>
+									<img src="/paper-plane-right.svg" alt="Odeslat" />
+								</Button>
+							</ModalFooter>
+						</>
+					)}
+				</ModalContent>
+			</Modal>
+		</>
+	);
 }
--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -6,8 +6,8 @@ import type { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 const openai = new OpenAI({ apiKey: import.meta.env.OPENAI_API_KEY });
 
 export async function GET({ request }: APIContext) {
-	//const messageArray = await request.json() as ChatCompletionMessageParam[];
-	const messageArray: ChatCompletionMessageParam[] = [{ role: "user", content: "Dobrý den, mám problém s kolenem." }];
+	const messageArray = await request.json() as ChatCompletionMessageParam[];
+	//const messageArray: ChatCompletionMessageParam[] = [{ role: "user", content: "Dobrý den, mám problém s kolenem." }];
 
 	const initialMessages: ChatCompletionMessageParam[] = [
 		{ role: "system", content: "Jsi chatbot, který má za úkol poradit uživatelům k jakému typu doktora si mají zajít se svým problémem, podle jejich specializace. Svoje odpovědi ponechej stručné, ale stále podej alespoň nějaké informace nevíc a nechoď zbytečne do detailů pokud to uživatel nechce. Nabídni uživateli více informací." }
@@ -20,6 +20,8 @@ export async function GET({ request }: APIContext) {
 
 	const headers = new Headers();
 	headers.append('Content-Type', 'application/json');
+
+	console.log("sending response");
 
 	return {
 		status: 200, // HTTP status code for "OK"
